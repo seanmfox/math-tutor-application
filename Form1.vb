52288af86f9419c63cd5dbcdf196bb06ee09303f
seanmfox <geekypghrunner@gmail.com>
2019-05-23 20:10:20 -0400

    Add identification information

--- Form1.vb
+++ Form1.vb
@@ -1,4 +1,10 @@
-﻿Public Class Form1
+﻿'Name: Sean Fox
+'Course: CIT-161-Z03B
+'Date: 5-22-19
+'Program Name: Math Tutor Application
+'Description: Chapter 2, Programming Challenge 2 - Create an application that, when a button is clicked, shows the answer to a math problem
+
+Public Class Form1
     Private Sub BtnShowAnswer_Click(sender As Object, e As EventArgs) Handles btnShowAnswer.Click
         lblSolution.Text = "30"
     End Sub
@@ -8,6 +14,10 @@
     End Sub
 
     Private Sub LblSolution_Click(sender As Object, e As EventArgs) Handles lblSolution.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
